edc7f7b72caf929bee5e619b0805f3eb02b5ea36
623466433 <623466433@qq.com>
2018-08-15 00:14:52 +0800

    添加了前端代码

---
+++ pages/member-info/member-info.js
@@ -0,0 +1,71 @@
+// pages/member-info.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    name: "王利民",
+    weight: "60kg",
+    height: "172cm",
+    gender: "男",
+    birthday: "1998年01月01日",
+    mobile: "123456789000"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
